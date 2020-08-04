8f41ce0aeb994e1f4782c209bde7b6ff98711ba3
madfalc0n <chadool116@naver.com>
2020-08-04 16:04:50 +0900

    update C00 ex06

--- C00/ex06/ft_print_comb2.c
+++ C00/ex06/ft_print_comb2.c
@@ -12,25 +12,41 @@
 
 #include <unistd.h>
 
-void	sub_print2(int numb)
-{
-	char a;
-	char b;
 
-	a = numb + 48;
-	write();
+void print_comb(char c)
+{
+	write(1, &c, 1);
 }
 
-void	ft_print_comb2(void)
+void sub_comb2(int a, int b)
 {
-	char numb[3];
-
-	numb[0] = 0;
-	numb[1] = ',';
-	numb[2] = ' ';
-	while (numb[0] <= 98)
+	print_comb(48 + a / 10);
+	print_comb(48 + a % 10);	
+	print_comb(' ');
+	print_comb(48 + b / 10);
+	print_comb(48 + b % 10);
+	if (!(a == 98 && b == 99))
 	{
-			
+		print_comb(',');
+		print_comb(' ');
+	}
+}
+
+void ft_print_comb2(void)
+{
+	int a;
+	int b;
+
+	a = 0;
+	while (a <= 98)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			sub_comb2(a,b);
+			b++;
+		}
+		a++;
 	}
 	
 
