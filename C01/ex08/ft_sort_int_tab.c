4ce4ffe4f5f807481cf7b8de0926949b8689875a
madfalc0n <chadool116@naver.com>
2020-08-07 23:29:39 +0900

    Update C01 ex08

--- C01/ex08/ft_sort_int_tab.c
+++ C01/ex08/ft_sort_int_tab.c
@@ -1,6 +1,26 @@
 
-
 void ft_sort_int_tab(int *tab, int size)
 {
+	int i;
+	int j;
+	int tmp;
+	
+	i = 0;
+	while (i < size - 1)
+	{
+		j = i + 1;
+		while (j < size)
+		{
+			if (tab[i] >= tab[j])
+			{
+				tmp = tab[j];
+				tab[j] = tab[i];
+				tab[i] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+		
 
 }
--- C01/ex08/main.c
+++ C01/ex08/main.c
@@ -1,9 +1,21 @@
+#include <stdio.h>
+
 void ft_sort_int_tab(int *tab, int size);
 
 int main(void)
 {
-	int a[] = {5,3,23,6,2,1};
+	int a[] = {5,3,9,6,2,1};
 	
+	for (int i = 0 ; i < 6 ; i++)
+	{
+		printf("%d", a[i]);
+	}
+	printf("\n");
 	ft_sort_int_tab(a,6);
+        for (int i = 0 ; i < 6 ; i++)
+        {
+                printf("%d", a[i]);
+        }
+	printf("\n");
 	return (0);
 }
