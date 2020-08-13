5e0bc6ff709f98114446b526aec01c6bfd21cdc5
madfalc0n <chadool116@naver.com>
2020-08-13 16:49:55 +0900

    Update C03 04

--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -4,22 +4,18 @@ char *ft_strstr(char *str, char *to_find)
     int j;
 
     i = 0;
-    if (!*to_find)
-        return (&str[0]);
-
+    if (*to_find == 0) return (&str[0]);
     while (str[i] != '\0')
     {
         if (str[i] == to_find[0])
         {
-            j = i;
+            j = 0;
             while (to_find[j] != '\0' && str[j] != '\0')
             {
-                if (!(str[j] == to_find[j]))
-                    break;
+                if (!(str[i + j] == to_find[j])) break;
                 j++;
             }
-            if (to_find[j] == '\0')
-                return (&str[i]);
+            if (to_find[j] == '\0') return (&str[i]);
         }
         i++;
     }
