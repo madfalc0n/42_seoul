ec9cd86ab2d3503e8bacb8102643ebe74ab04d07
madfalc0n <chadool116@naver.com>
2020-08-14 23:16:10 +0900

    Update C02 ex01

--- C02/ex01/ft_strncpy.c
+++ C02/ex01/ft_strncpy.c
@@ -6,24 +6,21 @@
 /*   By: myokim <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/10 15:39:43 by myokim            #+#    #+#             */
-/*   Updated: 2020/08/10 16:10:26 by myokim           ###   ########.fr       */
+/*   Updated: 2020/08/14 00:29:45 by myokim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
-char	*ft_strncpy(char *dest, char *src, unsigned int n)
+char				*ft_strncpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
-	char tmp;
-	char tmp2;
+	unsigned	int	i;
+	char			tmp;
+	char			tmp2;
 
 	i = 0;
 	while (i < n)
 	{
 		tmp = *dest;
 		tmp2 = *src;
-		printf("des : %c src : %c\n", tmp ,tmp2);
 		dest = &tmp2;
 		if (tmp2 == '\0')
 			return (dest);
@@ -32,12 +29,4 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 		i++;
 	}
 	return (dest);
-}
-
-int main(void)
-{
-	char dst[5] = "Hello";
-	char src[3] = "com";
-	
-	printf("%s\n", ft_strncpy(dst, src, 5));	
 }
